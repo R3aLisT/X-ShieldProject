ab8c15bf851828b8324c25f8f720d5a7feb37c38
Selçuk ÇUKUR <xLAZx@users.noreply.github.com>
2014-04-03 13:56:58 +0300

    GameServer: Yeni ITEM tablosu için gerekli işlemle
    r...

--- src/shared/database/ItemUpgradeSet.h
+++ src/shared/database/ItemUpgradeSet.h
@@ -1,37 +1,88 @@
 #pragma once
 
-class CItemUpgradeSet : public OdbcRecordset
+class CItemTableSet : public OdbcRecordset
 {
 public:
-	CItemUpgradeSet(OdbcConnection * dbConnection, ItemUpgradeArray * pMap) 
+	CItemTableSet(OdbcConnection * dbConnection, ItemtableArray * pMap) 
 		: OdbcRecordset(dbConnection), m_pMap(pMap) {}
 
-	virtual tstring GetTableName() { return _T("ITEM_UPGRADE"); }
-	virtual tstring GetColumns() { return _T("nIndex, nNPCNum, nOriginType, nOriginItem, "
-		"nReqItem1, nReqItem2, nReqItem3, nReqItem4, nReqItem5, nReqItem6, nReqItem7, nReqItem8, "
-		"nReqNoah, bRateType, nGenRate, nGiveItem"); }
+	virtual tstring GetTableName() { return _T("ITEM"); }
+	virtual tstring GetColumns() { return _T("Num, Ext, strName, StrDesc, IconID, Kind, Slot, Race, Class, Damage, Delay, Range, Weight, Duration, BuyPrice, SellPrice, Ac, Countable, Effect1, Effect2, ReqLevel, ReqLevelMax, ReqRank, ReqTitle, ReqStr, ReqSta, ReqDex, ReqIntel, ReqCha, SellingGroup, ItemType, Hitrate, Evasionrate, DaggerAc, SwordAc, MaceAc, AxeAc, SpearAc, BowAc, FireDamage, IceDamage, LightningDamage, PoisonDamage, HPDrain, MPDamage, MPDrain, MirrorDamage, StrB, StaB, DexB, IntelB, ChaB, MaxHpB, MaxMpB, FireR, ColdR, LightningR, MagicR, PoisonR, CurseR, ItemClass, ItemSetID, GiveItem"); }
 
 	virtual bool Fetch()
 	{
-		_ITEM_UPGRADE *pData = new _ITEM_UPGRADE;
+		_ITEM_TABLE *pData = new _ITEM_TABLE;
 
 		int i = 1;
-		_dbCommand->FetchUInt32(i++, pData->nIndex);
-		_dbCommand->FetchUInt16(i++, pData->sNpcNum);
-		_dbCommand->FetchSByte(i++, pData->bOriginType);
-		_dbCommand->FetchUInt16(i++, pData->sOriginItem);
-		for (int x = 0; x < MAX_ITEMS_REQ_FOR_UPGRADE; x++)
-			_dbCommand->FetchUInt32(i++, pData->nReqItem[x]);
-		_dbCommand->FetchUInt32(i++, pData->nReqNoah);
-		_dbCommand->FetchByte(i++, pData->bRateType);
-		_dbCommand->FetchUInt16(i++, pData->sGenRate);
-		_dbCommand->FetchInt32(i++, pData->nGiveItem);
+		_dbCommand->FetchUInt32(i++, pData->m_iNum);
+		_dbCommand->FetchByte(i++, pData->m_bExt);
+		_dbCommand->FetchString(i++, pData->m_sName);
+		_dbCommand->FetchString(i++, pData->m_sDesc);
+		_dbCommand->FetchByte(i++, pData->m_bIconID);
+		_dbCommand->FetchByte(i++, pData->m_bKind);
+		_dbCommand->FetchByte(i++, pData->m_bSlot);
+		_dbCommand->FetchByte(i++, pData->m_bRace);
+		_dbCommand->FetchByte(i++, pData->m_bClass);
+		_dbCommand->FetchUInt16(i++, pData->m_sDamage);
+		_dbCommand->FetchUInt16(i++, pData->m_sDelay);
+		_dbCommand->FetchUInt16(i++, pData->m_sRange);
+		_dbCommand->FetchUInt16(i++, pData->m_sWeight);
+		_dbCommand->FetchUInt16(i++, pData->m_sDuration);
+		_dbCommand->FetchUInt32(i++, pData->m_iBuyPrice);
+		_dbCommand->FetchUInt32(i++, pData->m_iSellPrice);
+		_dbCommand->FetchInt16(i++, pData->m_sAc);
+		_dbCommand->FetchByte(i++, pData->m_bCountable);
+		_dbCommand->FetchUInt32(i++, pData->m_iEffect1);
+		_dbCommand->FetchUInt32(i++, pData->m_iEffect2);
+		_dbCommand->FetchByte(i++, pData->m_bReqLevel);
+		_dbCommand->FetchByte(i++, pData->m_bReqLevelMax);
+		_dbCommand->FetchByte(i++, pData->m_bReqRank);
+		_dbCommand->FetchByte(i++, pData->m_bReqTitle);
+		_dbCommand->FetchByte(i++, pData->m_bReqStr);
+		_dbCommand->FetchByte(i++, pData->m_bReqSta);
+		_dbCommand->FetchByte(i++, pData->m_bReqDex);
+		_dbCommand->FetchByte(i++, pData->m_bReqIntel);
+		_dbCommand->FetchByte(i++, pData->m_bReqCha);
+		_dbCommand->FetchByte(i++, pData->m_bSellingGroup);
+		_dbCommand->FetchByte(i++, pData->m_ItemType);
+		_dbCommand->FetchUInt16(i++, pData->m_sHitrate);
+		_dbCommand->FetchUInt16(i++, pData->m_sEvarate);
+		_dbCommand->FetchUInt16(i++, pData->m_sDaggerAc);
+		_dbCommand->FetchUInt16(i++, pData->m_sSwordAc);
+		_dbCommand->FetchUInt16(i++, pData->m_sMaceAc);
+		_dbCommand->FetchUInt16(i++, pData->m_sAxeAc);
+		_dbCommand->FetchUInt16(i++, pData->m_sSpearAc);
+		_dbCommand->FetchUInt16(i++, pData->m_sBowAc);
+		_dbCommand->FetchByte(i++, pData->m_bFireDamage);
+		_dbCommand->FetchByte(i++, pData->m_bIceDamage);
+		_dbCommand->FetchByte(i++, pData->m_bLightningDamage);
+		_dbCommand->FetchByte(i++, pData->m_bPoisonDamage);
+		_dbCommand->FetchByte(i++, pData->m_bHPDrain);
+		_dbCommand->FetchByte(i++, pData->m_bMPDamage);
+		_dbCommand->FetchByte(i++, pData->m_bMPDrain);
+		_dbCommand->FetchByte(i++, pData->m_bMirrorDamage);
+		_dbCommand->FetchInt16(i++, pData->m_sStrB);
+		_dbCommand->FetchInt16(i++, pData->m_sStaB);
+		_dbCommand->FetchInt16(i++, pData->m_sDexB);
+		_dbCommand->FetchInt16(i++, pData->m_sIntelB);
+		_dbCommand->FetchInt16(i++, pData->m_sChaB);
+		_dbCommand->FetchInt16(i++, pData->m_MaxHpB);
+		_dbCommand->FetchInt16(i++, pData->m_MaxMpB);
+		_dbCommand->FetchInt16(i++, pData->m_bFireR);
+		_dbCommand->FetchInt16(i++, pData->m_bColdR);
+		_dbCommand->FetchInt16(i++, pData->m_bLightningR);
+		_dbCommand->FetchInt16(i++, pData->m_bMagicR);
+		_dbCommand->FetchInt16(i++, pData->m_bPoisonR);
+		_dbCommand->FetchInt16(i++, pData->m_bCurseR);
+	        _dbCommand->FetchByte(i++,  pData->m_bItemClass);
+		_dbCommand->FetchByte(i++,  pData->m_bItemSetID);
+		_dbCommand->FetchByte(i++,  pData->m_bGiveItem);
 
-		if (!m_pMap->PutData(pData->nIndex, pData))
+		if (!m_pMap->PutData(pData->m_iNum, pData))
 			delete pData;
 
 		return true;
 	}
 
-	ItemUpgradeArray *m_pMap;
+	ItemtableArray *m_pMap;
 };
